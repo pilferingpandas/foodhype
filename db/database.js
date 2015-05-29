b6efbef833df838c4c9000280166831472db17ca
David Goldberg <drgold1988@gmail.com>
2015-05-29 12:27:00 -0700

    feat: database schema exists in modular form

--- db/database.js
+++ db/database.js
@@ -5,13 +5,39 @@ var keys = require('../config/panda-config.js');
 // Opens a connection to the test database on our locally running instance of MongoDB
 mongoose.connect('mongodb://' + keys.mongo.username + ':' + keys.mongo.password + '@ds037272.mongolab.com:37272/pilferingpandas');
 
-
-// There is a pending connection to the test database running on localhost
 // Notify if we connect successfully or if a connection error occurs
 var db = mongoose.connection;
+
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function(callback) {
-    console.log('were open');
+db.once('open', function() {
+    console.log('Mongodb connection is open');
 });
+
+module.exports = db;
+
+// var restaurantSchema = mongoose.Schema({
+//   name: String,
+//   url: String,
+//   score: {type: Number, min: 1, max: 17},
+//   address: Schema.Types.Mixed,
+//   latitude: Number, // TODO: determine if we need to calculate decimals and to what number do we round after the deimal point
+//   longitude: Number, // TODO: determine if we need to calculate decimals and to what number do we round after the deimal point
+//   numberReviews: Number
+// });
+
+// var Restaurant = mongoose.model('Restuarant', restaurantSchema);
+
+// var citySchema = mongoose.Schema({
+//   name: String,
+//   state: String,
+//   latitude: Number, // TODO: determine if we need to calculate decimals and to what number do we round after the deimal point
+//   longitude: Number // TODO: determine if we need to calculate decimals and to what number do we round after the deimal point
+// })
+
+// var City = mongoose.model('City', citySchema);
+
+
+// module.exports = Restuarant;
+// module.exports = City;
 
 
