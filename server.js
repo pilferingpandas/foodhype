5ef9b625f7466a32792a14a9ca009ccdffc9412b
kenning <hwerzog@gmail.com>
2015-05-28 22:22:02 -0700

    feat: add favicon

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var favicon = require('serve-favicon');
 var app = express();
 
 app.use(express.static(__dirname + '/client'));
+app.use(favicon(__dirname + '/client/favicon.ico'));
 
 var port = 3000;
 app.listen(port);
