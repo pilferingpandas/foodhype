96c77e993c053b71353e6b2d2d7f2e7842722dac
Dominika <bienkowska.dominika@gmail.com>
2015-06-02 20:08:01 -1000

    implements instagram returning media count and url for 10 restaurants

--- cron/inst.js
+++ cron/inst.js
@@ -1,64 +1,58 @@
 var keys = require('../config/panda-config.js');
-var express = require('express');
-var app = require('../server.js')
 var instagram = require('instagram-node-lib');
 var yelp = require('./yelp.js');
 instagram.set('client_id', keys.instagram.client_id);
 instagram.set('client_secret', keys.instagram.client_secret);
 
-
-
 var restaurants = yelp.testGetTenRestaurants(function(restaurantsFromYelp){
- console.log(restaurantsFromYelp[0].name)
-
- for (var i=0; i<restaurantsFromYelp.length; i++){
-    // check instagram for every restaurant (provide name + coordinates)
-    // console.log(restaurants[i].name);
-    var resName = restaurantsFromYelp[i].name;
-    // escape single quotes in the name
-    resName = resName.replace(/'/g, "");
-    // join words into one string
-    resName = resName.replace(/ /g, "");
-    resName = resName.toLowerCase();
-    var latitude = Math.ceil(restaurantsFromYelp[i].latitude);
-    var count;
-    var url;
-   // console.log('instagram search for', resName)
-   instagram.tags.recent({ name: resName, 
-     complete: function(data){
-      count=0;
-    // iterate through the data array and check if coordinates are the same as yelp cors
-    for ( var i=0 ; i<data.length; i++){
-      
-      if (data[i].location){
-        if (Math.ceil(data[i].location.latitude) === latitude){
-          //console.log('found');
-          if (count === 0){
-            url = data[i].link;
-          }
-          count++;
-        } else {
-          //console.log(data[i].location)
-        }
+     for (var i=0; i<restaurantsFromYelp.length; i++){
+        var restaurantName = restaurantsFromYelp[i].name;
+        // escape single quotes in the name
+        
+        var latitude = Math.ceil(restaurantsFromYelp[i].latitude);
+        module.exports.getApiData(restaurantName, latitude, console.log)
       }
-    }
-    console.log('found ',count, 'restaurants ', url);
-    // return count, url;
-  }
-});
-   
- }
-
 });
 
 
-
-// Fill me out!
+// Fill me out! 
 module.exports = {
-  getApiData : function(thisYelpData, callback) {
+  getApiData : function(restaurantName, latitude, callback) {
     // Contact the API with thisYelpData, then...
-
-    callback(dataReturnedFromApi);
+    resName = restaurantName.replace(/'/g, "");
+    // join words into one string
+    resName = resName.replace(/ /g, "").toLowerCase();
+    instagram.tags.recent({ name: resName, 
+      complete: function(data){
+        var count = 0; 
+        var url = null;
+        console.log('contacting instagram for', resName)
+        
+        // iterate through the data array and check if coordinates are the same as yelp cors
+        for ( var i=0 ; i<data.length; i++){
+              if (data[i].location){
+                    if (Math.ceil(data[i].location.latitude) === latitude){
+                          //console.log('found');
+                          if (count === 0){
+                            // url = data[i].link;// this would return instagram profile page
+                            // url = data[i].images.standard_resolution.url
+                            // for thumbnail
+                            url = data[i].images.thumbnail.url
+                          }
+                          count++;
+                    } else {
+                      // the location does not match location of restaurant
+                      //console.log(data[i].location)
+                    }
+              }
+        }
+        var dataFromInstagram = {
+          numPics : count,
+          url : url
+        }
+        callback(dataFromInstagram);
+    }
+    });
   }
 }
 
